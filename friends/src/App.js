1a23f99ec5d470989454ab9d5f4a2cba47c68644
Scott Renz <scottrenz@scottrenz.com>
2019-08-20 17:51:33 -0400

    showing friends

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Login from './components/Login';
+import AppFriends from './components/AppFriends';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Route path="/login" component={Login} />
+        <PrivateRoute exact path="/protected" component={AppFriends} />
+        {/* <PrivateRoute path="/anotherRoute" component={SomeOtherComponent} /> */}
+      </div>
+    </Router>
   );
 }
 
