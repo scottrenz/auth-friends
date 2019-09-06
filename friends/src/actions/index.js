16fedbbb5ecfc031070526cc4f1fbf03c5937fc8
Scott Renz <scottrenz@scottrenz.com>
2019-09-05 20:35:44 -0400

    remove /

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -13,7 +13,7 @@ export const DELETING_FRIEND = 'DELETING_FRIEND';
 export const SINGLE_FRIEND = 'SINGLE_FRIEND';
 export const TOGGLE_UPDATE_FRIEND = 'TOGGLE_UPDATE_FRIEND';
 
-const URL = 'https://smurfs-scott.herokuapp.com/';
+const URL = 'https://smurfs-scott.herokuapp.com';
 export const getFriends = () => {
   const friends = axiosWithAuth().get(`${URL}`);
   return dispatch => {
